df8deb218f946bb0e1242406491185aa8afdf67a
NawaNawa <ltue2129@gmail.com>
2019-04-04 00:55:15 +0800

    addRemoveListener Ok

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 new ClipboardJS('.btn.copyBtn');
 import {Pedestrian} from "./pedestrian.js";
 import {PedestrianList} from "./pedestrianList.js";
-import {Coordinate} from "./utl.js";
+import {Coordinate,isset,isnotset} from "./utl.js";
 
 class View
 {
@@ -9,7 +9,9 @@ class View
     {
         this.inputs={};
         this.buttons={};
+        this.data={};
         this.onChange=()=>{};
+        this._onInputParseError=sender=>this.onInputParseError(sender);
     }
     init()
     {
@@ -34,13 +36,39 @@ class View
             this["on"+key+"Change"]=()=>{};
             this["_on"+key+"Change"]=function(e)
             {
-                that["on"+key+"Change"](parseFloat(this.value),e);
+                var key=this.dataset.key;
+                var val=undefined;
+                try{
+                    val=parseFloat(this.value);
+                }
+                catch(e)
+                {that._onInputParseError(this);}
+                that.data[key]=val;
+                that["on"+key+"Change"](val,e);
                 that._onChange();
             }
             var input=this.inputs[key];
+            input.dataset.key=key;
             var func=this["_on"+key+"Change"];
             input.addEventListener("change",func);
         }
+        for(var key in this.buttons)
+        {
+            var button=this.buttons[key];
+            //button.dataset.key=key;
+            this["on"+key+"Click"]=()=>{};
+            this["_on"+key+"Click"]=function(e)
+            {
+                var key=this.dataset.key;
+                that["on"+this.dataset.key+"Click"](e);
+            }
+            button.addEventListener("click",this["_on"+key+"Click"]);
+            button.dataset.key=key;
+        }
+    }
+    onInputParseError(sender)
+    {
+
     }
     get output()
     {
@@ -62,7 +90,26 @@ class Controller
         this.view=new View();
         this.view.init();
         this.model= this.pedestrains =new PedestrianList();
+        this.init();
     }
+    init()
+    {
+        this.attachEventListener();
+    }
+    attachEventListener()
+    {
+        this.view.onaddClick=this.onAdd;
+        this.view.onremoveClick=this.onRemove;
+    }
+    onAdd()
+    {
+        console.log("add");
+    }
+    onRemove()
+    {
+        console.log("remove");
+    }
+
     
 }
 window.app={}
